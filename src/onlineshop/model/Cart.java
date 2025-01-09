816864bf28a6d3d8265e164ac45ccea4d82a5d79
Pawel Zalewski <pawel.zalewski@360softwarehouse.com>
2025-01-09 11:32:03 +0100

    klasa Cart z funkcjami dodawania, usuwania, wyswietlania produktow i obliczanie sumy cen produktow w koszyku

---
+++ src/onlineshop/model/Cart.java
@@ -0,0 +1,33 @@
+package onlineshop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println("Dodano do koszyka: " + product.getName());
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+        System.out.println("Usunięto z koszyka: " + product.getName());
+    }
+
+    public void displayCart() {
+        if (products.isEmpty()) {
+            System.out.println("Koszyk jest pusty.");
+        } else {
+            System.out.println("Produkty w koszyku:");
+            products.forEach(product -> product.getInfo());
+        }
+    }
+
+    public double calculateTotal() {
+        return products.stream()
+                .mapToDouble(p -> p.getPrice())
+                .sum();
+    }
+}
